412b4aa8e3697bc26c6a8a7327c4a2ef0f0c2cfb
Rafael Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-07-02 17:28:34 -0300

    Inheritance

---
+++ Inheritance and Polymorphism/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Polymorphism.Entities
+{
+    internal class Account
+    {
+
+
+        public int Number { get; protected set; } // o protected serve para dar acesso apinas para as subclasses dentro e fora do assembly
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double Amount)
+        {
+            Balance -= Amount;
+        }
+
+        public void Deposit(double Amount)
+        {
+            Balance += Amount; 
+        } 
+    }
+}
---
+++ Inheritance and Polymorphism/Entities/BusinessAccount.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Inheritance_and_Polymorphism.Entities
+{
+    internal class BusinessAccount : Account // herança, a classe BusinessAccount vai ter as mesmas funções e dados da classe Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        { 
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
